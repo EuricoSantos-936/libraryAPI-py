21eb222de6f847dc662fbe083c9097ab2746a673
Bizhead <euricosantos_936@hotmail.com>
2023-09-27 16:17:05 +0100

    Find book by id

--- app.py
+++ app.py
@@ -13,9 +13,18 @@ books = [
 # Search all books
 
 
-@app.route("/books")
+@app.route("/books", method=["GET"])
 def get_books():
     return jsonify(books)
+
+# Find book by id
+
+
+@app.route('/books/<int:id>', method=["GET"])
+def get_book_bt_id(id):
+    for book in books:
+        if book.get('id') == id:
+            return jsonify(book)
 
 
 app.run(port=5000, host='localhost', debug=True)
