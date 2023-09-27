1ebc0fb258893f0d407bb8e2b33114f62d88eda2
Bizhead <euricosantos_936@hotmail.com>
2023-09-27 16:48:59 +0100

    Add function "Modify book"
    put type ignore on route because have error on type on vscode

--- app.py
+++ app.py
@@ -13,18 +13,27 @@ books = [
 # Search all books
 
 
-@app.route("/books", method=["GET"])
+@app.route("/books", methods=["GET"])
 def get_books():
     return jsonify(books)
 
 # Find book by id
 
 
-@app.route('/books/<int:id>', method=["GET"])
+@app.route("/books/<int:id>", methods=["GET"]) # type: ignore
 def get_book_bt_id(id):
     for book in books:
         if book.get('id') == id:
             return jsonify(book)
 
+#  Modify book
+
+@app.route('/books/<int:id>', methods=["PUT"]) # type: ignore
+def modify_book(id):
+    modified_book = request.get_json()
+    for index,book in enumerate(books):
+        if book.get('id') == id:
+            books[index].update(modified_book)
+            return jsonify(books[index])
 
 app.run(port=5000, host='localhost', debug=True)
